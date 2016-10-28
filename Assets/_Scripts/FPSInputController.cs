9bed0dd3e755e5f9d9d62e7d821428a02c47e946
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2016-10-28 15:09:19 -0400

    Add start button to scene

--- Assets/_Scripts/FPSInputController.cs
+++ Assets/_Scripts/FPSInputController.cs
@@ -1,7 +1,8 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 // Require a character controller to be attached to the same game object
 [RequireComponent(typeof(CharacterMotor))]
@@ -13,8 +14,13 @@ public class FPSInputController : MonoBehaviour
 	public bool checkAutoWalk = false;
 	private GameObject head;
 
+	public Button startGameButton;
 	public Text timerText;
 	float timeLeft;
+	Scene currentScene;
+	bool isGameActive = false;
+
+	public GamePlayMenu gameplayMenu;
 
 
 	private bool MFI_Connected = false;
@@ -47,65 +53,73 @@ public class FPSInputController : MonoBehaviour
 	// Use this for initialization
 	void Awake()
 	{
+		currentScene = SceneManager.GetActiveScene ();
 		motor = GetComponent<CharacterMotor>();
 		StartCoroutine(CheckForControllers());
 		head = GameObject.FindWithTag ("GvrHead");
+		if (!isGameActive) {
+			startGameButton.gameObject.SetActive (true);
+		}
 
-		StartGame ();
-
+		Debug.Log ("Scene count " + SceneManager.sceneCount);
+		Debug.Log ("Current Scene " + currentScene.name);
 	}
-	
+
 	// Update is called once per frame
 	void Update()
 	{
-		
-		// Set forward direction toward camera
-//		transform.eulerAngles.y = head.transform.rotation.y; 		// Get the input vector from keyboard or analog stick
-		Debug.Log(head.transform.localEulerAngles.y);
-		Vector3 directionVector;
-		if (!checkAutoWalk) { 
-			directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		} else { 
-			directionVector = new Vector3(0, 0, 1);
+		if (isGameActive) {
+			// Set forward direction toward camera
+			//		transform.eulerAngles.y = head.transform.rotation.y; 		// Get the input vector from keyboard or analog stick
+			//		Debug.Log(head.transform.localEulerAngles.y);
+			Vector3 directionVector;
+			if (!checkAutoWalk) {
+				directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			} else {
+				directionVector = new Vector3(0, 0, 1);
+			}
+
+			if (directionVector != Vector3.zero)
+			{
+				// Get the length of the directon vector and then normalize it
+				// Dividing by the length is cheaper than normalizing when we already have the length anyway
+				float directionLength = directionVector.magnitude;
+				directionVector = directionVector / directionLength;
+
+				// Make sure the length is no bigger than 1
+				directionLength = Mathf.Min(1.0f, directionLength);
+
+				// Make the input vector more sensitive towards the extremes and less sensitive in the middle
+				// This makes it easier to control slow speeds when using analog sticks
+				directionLength = directionLength * directionLength;
+
+				// Multiply the normalized direction vector by the modified length
+				directionVector = directionVector * directionLength;
+			}
+
+			// Apply the direction to the CharacterMotor
+			motor.inputMoveDirection = head.transform.rotation * directionVector;
+			motor.inputJump = Input.GetButton("Jump");
+
+			if (Input.GetButton("Fire1")) {
+				//			Debug.Log ("PRESSED!!!");
+			};
+
+			RunTimer ();
 		}
 
-		if (directionVector != Vector3.zero)
-		{
-			// Get the length of the directon vector and then normalize it
-			// Dividing by the length is cheaper than normalizing when we already have the length anyway
-			float directionLength = directionVector.magnitude;
-			directionVector = directionVector / directionLength;
-			
-			// Make sure the length is no bigger than 1
-			directionLength = Mathf.Min(1.0f, directionLength);
-			
-			// Make the input vector more sensitive towards the extremes and less sensitive in the middle
-			// This makes it easier to control slow speeds when using analog sticks
-			directionLength = directionLength * directionLength;
-			
-			// Multiply the normalized direction vector by the modified length
-			directionVector = directionVector * directionLength;
-		}
-		
-		// Apply the direction to the CharacterMotor
-		motor.inputMoveDirection = head.transform.rotation * directionVector;
-		motor.inputJump = Input.GetButton("Jump");
-
-		if (Input.GetButton("Fire1")) {
-			Debug.Log ("PRESSED!!!");
-		};
-
-		RunTimer ();
 	}
 
 	void GameOver()
 	{
-		
+
 	}
 
-	void StartGame()
+	public void StartGame()
 	{
+		startGameButton.gameObject.SetActive (false);
 		timeLeft = 60.0f;
+		isGameActive = true;
 	}
 
 	void SetTimerText()
